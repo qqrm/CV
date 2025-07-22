25cbddfcffbb21b04ea87cc97dbb88673da061b5
Alexey <4681325+qqrm@users.noreply.github.com>
2025-07-22 20:38:48 +0300

    Rename html_template to html_template_en

--- sitegen/src/main.rs
+++ sitegen/src/main.rs
@@ -11,7 +11,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     html_body = html_body.replace("./latex/", "latex/");
     html_body = html_body.replace("./README_ru.md", "ru/");
 
-    let html_template = format!(
+    let html_template_en = format!(
         "<!DOCTYPE html>\n<html lang='en'>\n<head>\n    <meta charset='UTF-8'>\n    <title>Alexey Belyakov - CV</title>\n    <link rel='stylesheet' href='style.css'>\n</head>\n<body>\n<div class='content'>\n{}\n</div>\n<footer>\n    <p><a href='latex/en/Belyakov_en.pdf'>Download PDF (EN)</a></p>\n    <p><a href='latex/ru/Belyakov_ru.pdf'>Скачать PDF (RU)</a></p>\n</footer>\n</body>\n</html>\n",
         html_body
     );
@@ -32,7 +32,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     if !docs_dir.exists() {
         fs::create_dir_all(docs_dir)?;
     }
-    fs::write(docs_dir.join("index.html"), html_template)?;
+    fs::write(docs_dir.join("index.html"), html_template_en)?;
 
     let ru_dir = docs_dir.join("ru");
     if !ru_dir.exists() {
