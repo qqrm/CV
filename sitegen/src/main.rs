ad04faaa07c75dfa97b716a8e6fc105f3143da6e
Alexey <4681325+qqrm@users.noreply.github.com>
2025-07-22 22:12:58 +0300

    fix: remove duplicate name on site

--- sitegen/src/main.rs
+++ sitegen/src/main.rs
@@ -11,11 +11,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     push_html(&mut html_body, parser);
     html_body = html_body.replace("./latex/", "latex/");
     html_body = html_body.replace("./README_ru.md", "ru/");
+    if let Some(end) = html_body.find("</h1>") {
+        html_body = html_body[end + 5..].trim_start().to_string();
+    }
 
     let html_template = format!(
         "<!DOCTYPE html>\n<html lang='en'>\n<head>\n    <meta charset='UTF-8'>\n    <title>Alexey Belyakov - CV</title>\n    <link rel='stylesheet' href='style.css'>\n</head>\n<body>\n<div class='content'>\n<img class='avatar' src='{}' alt='Avatar'>\n{}\n</div>\n<footer>\n    <p><a href='latex/en/Belyakov_en.pdf'>Download PDF (EN)</a></p>\n    <p><a href='latex/ru/Belyakov_ru.pdf'>Скачать PDF (RU)</a></p>\n</footer>\n</body>\n</html>\n",
-        AVATAR_URL,
-        html_body
+        AVATAR_URL, html_body
     );
 
     // Generate Russian version
@@ -24,11 +26,13 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut html_body_ru = String::new();
     push_html(&mut html_body_ru, parser_ru);
     html_body_ru = html_body_ru.replace("./latex/", "../latex/");
+    if let Some(end) = html_body_ru.find("</h1>") {
+        html_body_ru = html_body_ru[end + 5..].trim_start().to_string();
+    }
 
     let html_template_ru = format!(
         "<!DOCTYPE html>\n<html lang='ru'>\n<head>\n    <meta charset='UTF-8'>\n    <title>Алексей Беляков - Резюме</title>\n    <link rel='stylesheet' href='../style.css'>\n</head>\n<body>\n<header>\n    <h1>Алексей Беляков</h1>\n</header>\n<div class='content'>\n<img class='avatar' src='{}' alt='Avatar'>\n{}\n</div>\n<footer>\n    <p><a href='../latex/en/Belyakov_en.pdf'>Download PDF (EN)</a></p>\n    <p><a href='../latex/ru/Belyakov_ru.pdf'>Скачать PDF (RU)</a></p>\n</footer>\n</body>\n</html>\n",
-        AVATAR_URL,
-        html_body_ru
+        AVATAR_URL, html_body_ru
     );
 
     let docs_dir = Path::new("docs");
