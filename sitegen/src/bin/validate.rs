c8ac7ab29a64391eb5cb62dd508ac022861f11fd
History Collapser <actions@github.com>
2025-11-07 23:43:26 +0000

    Snapshot: ef1bdd3423ded2c3b184fcbb53d6c682f8886094

---
+++ sitegen/src/bin/validate.rs
@@ -0,0 +1,12 @@
+use log::info;
+use std::error::Error;
+use std::fs;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+    info!("Starting validation");
+    fs::read_to_string("profiles/cv/en/CV.MD")?;
+    fs::read_to_string("profiles/cv/ru/CV_RU.MD")?;
+    info!("Validation successful");
+    Ok(())
+}
